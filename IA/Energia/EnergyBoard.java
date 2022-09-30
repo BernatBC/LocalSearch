fa82088454e6697cbb71923024def50935c51053
alexland7219 <alexland7219@gmail.com>
2022-09-30 13:42:07 +0200

    Started Board function

--- IA/Energia/EnergyBoard.java
+++ IA/Energia/EnergyBoard.java
@@ -1,4 +1,5 @@
 package IA.Energia;
+import java.lang.Math;
 
 public class EnergyBoard {
 
@@ -16,5 +17,44 @@ public class EnergyBoard {
 
     public EnergyBoard getClone(){
         return (EnergyBoard) this.clone();
+    }
+
+    public void assign_ng(int c, int k){
+        // Asignar la central k al cliente c 
+        // PRE: El cliente c no tiene central si se va a asignar, o tiene si se va a desasignar 
+        // PRE: El cliente es no garantizado
+        state[c] = k;
+    }
+
+    public boolean canAssign_ng(int c, int k){
+        if (clientes[c].getTipo() == Clientes.GARANTIZADO) return false;
+
+        double energyleft = energyLeft(k);
+
+        if (energyleft < clientes[c].getConsumo()*(1. + VEnergia.getPerdida(getDistance(c, k))))
+            return false;
+        else 
+            return true;
+    }
+
+    public double energyLeft(int k){
+        double eIni = centrales[k].getProduccion();
+        
+        for (int c = 0; c < clientes.length; ++c){
+            if (state[c] != k) continue;
+
+            eIni -= (clientes[c].getConsumo()*(1. + VEnergia.getPerdida(getDistance(c, k))));
+        }
+
+        return eIni;
+    }
+
+    public double getDistance(int c, int k){
+        double cx = clientes[c].getCoordX();
+        double cy = clientes[c].getCoordY();
+        double kx = centrales[k].getCoordX();
+        double ky = centrales[k].getCoordY();
+
+        return Math.sqrt((cx-kx)*(cx-kx) + (cy-ky)*(cy-ky));
     }
 }
