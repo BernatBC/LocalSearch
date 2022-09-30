1d7ac0dd3b10fdb2ae6b5b4e4a64978da3f2e44a
alexland7219 <alexland7219@gmail.com>
2022-09-30 16:34:59 +0200

    Added 'swap', 'canSwap' and finished 'assign'

--- IA/Energia/EnergyBoard.java
+++ IA/Energia/EnergyBoard.java
@@ -19,15 +19,17 @@ public class EnergyBoard {
         return (EnergyBoard) this.clone();
     }
 
-    public void assign_ng(int c, int k){
-        // Asignar la central k al cliente c 
+    public void assign(int c, int k){
+        // Asignar la central k (posiblemente k = -1 (quitar central) ) al cliente c 
         // PRE: El cliente c no tiene central si se va a asignar, o tiene si se va a desasignar 
         // PRE: El cliente es no garantizado
         state[c] = k;
     }
 
-    public boolean canAssign_ng(int c, int k){
+    public boolean canAssign(int c, int k){
         if (clientes[c].getTipo() == Clientes.GARANTIZADO) return false;
+
+        if (state[c] != -1 && k == -1) return true;
 
         double energyleft = energyLeft(k);
 
@@ -56,5 +58,36 @@ public class EnergyBoard {
         double ky = centrales[k].getCoordY();
 
         return Math.sqrt((cx-kx)*(cx-kx) + (cy-ky)*(cy-ky));
+    }
+
+    public boolean canSwap(int c1, int c2){
+        if (state[c1] == state[c2]) return false;
+        
+        int k1 = state[c1]; 
+        int k2 = state[c2];
+
+        if (k1 == -1 && clientes[c2].getTipo() == Clientes.GARANTIZADO) return false;
+        if (k2 == -1 && clientes[c1].getTipo() == Clientes.GARANTIZADO) return false;
+
+        double energyleft1 = energyLeft(k1) - clientes[c1].getConsumo()*(1. + VEnergia.getPerdida(getDistance(c1, k1)));
+        if (k1 != -1)
+            if (energyleft1 < clientes[c2].getConsumo()*(1. + VEnergia.getPerdida(getDistance(c2, k1))))
+                return false;
+
+        double energyleft2 = energyLeft(k2) - clientes[c2].getConsumo()*(1. + VEnergia.getPerdida(getDistance(c2, k2)));
+        if (k2 != -1)
+            if (energyleft2 < clientes[c1].getConsumo()*(1. + VEnergia.getPerdida(getDistance(c1, k2))))
+                return false;
+
+        return true;
+    }
+
+    public void swap(int c1, int c2){
+        // El client c1 passarà a tenir la central de c2 i el client c2 passarà a tenir la central del client c1
+        // És possible que c1 o c2 no tinguin central, pero almenys una n'ha de tenir
+        // central de c1 != central de c2,   s'han de seguir complint totes les restriccions de les solucions
+        int k = state[c1];
+        state[c1] = state[c2];
+        state[c2] = k;
     }
 }
